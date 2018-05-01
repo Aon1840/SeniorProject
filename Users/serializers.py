3cc926b1e6cbe2a7ca37fa8ed2de74b2e1cc6eb0
Aon1840 <aonattapon1840.ap@gmail.com>
2018-05-02 00:56:10 +0700

    Edit serializer to ModelSerializer

--- Users/serializers.py
+++ Users/serializers.py
@@ -1,17 +1,23 @@
 from rest_framework import serializers
 from Users.models import User
 
-class UserSerializer(serializers.Serializer):
-    user_id = serializers.IntegerField(read_only=True)
-    username = serializers.CharField(required=True,allow_blank=False,unique=True,max_length=15)
-    password = serializers.CharField(required=True,allow_blank=False,max_length=20)
-    name = serializers.CharField(required=True,allow_blank=False,max_length=30)
-    surname = serializers.CharField(required=True,allow_blank=False,max_length=30)
-    tel = serializers.CharField(required=True,allow_blank=False,max_length=10)
-    email = serializers.EmailField(required=True,allow_blank=False,unique=True)
-    register_date = serializers.DateTimeField(auto_now_add=True)
+# class UserSerializer(serializers.Serializer):
+#     user_id = serializers.IntegerField(read_only=True)
+#     username = serializers.CharField(required=True,allow_blank=False,max_length=15)
+#     password = serializers.CharField(required=True,allow_blank=False,max_length=20)
+#     name = serializers.CharField(required=True,allow_blank=False,max_length=30)
+#     surname = serializers.CharField(required=True,allow_blank=False,max_length=30)
+#     tel = serializers.CharField(required=True,allow_blank=False,max_length=10)
+#     email = serializers.EmailField(required=True,allow_blank=False)
+#     register_date = serializers.DateTimeField()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        field = ('user_id','username','password','name','surname','tel','email','register_date');
 
     def create(self, validated_data): #method for create and return new 'User' instance
+
         return User.objects.create(**validated_data)
 
     def update(self, instance, validated_data): #method for update and return exist 'User' instance
@@ -21,3 +27,6 @@ class UserSerializer(serializers.Serializer):
         instance.surname = validated_data.get('surname', instance.surname)
         instance.tel = validated_data.get('tel', instance.tel)
         instance.email = validated_data.get('email', instance.email)
+        instance.save()
+
+        return instance

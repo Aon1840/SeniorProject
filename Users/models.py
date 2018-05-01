3f4272076c64f4bb67bb3cfdb493da7ae81e4e6d
Aon1840 <aonattapon1840.ap@gmail.com>
2018-05-02 00:27:12 +0700

    Add Users model

--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=15,unique=True,blank=False)
+    password = models.CharField(max_length=20,blank=False)
+    name = models.CharField(max_length=30,blank=False)
+    surname = models.CharField(max_length=30,blank=False)
+    tel = models.CharField(max_length=10,blank=False)
+    email = models.EmailField(max_length=50,unique=True,blank=False)
+    register_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('user_id',)
